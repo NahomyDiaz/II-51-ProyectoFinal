a60a8c1cbc2e0a79943cdbf3179cf1a905fb5094
Nahomy <diaznahomy341@gmail.com>
2025-11-29 12:05:26 -0600

    Implement form handling for student registration and enhance UI styles

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,11 +1,8 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-// ==========================
-// CONFIGURA TU SUPABASE
-// ==========================
-let SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlxZXB5anFsbmxid3R1ZWhybmdyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQzNTg2MDQsImV4cCI6MjA3OTkzNDYwNH0.C-WwzpilT9jUsVXhoOLjDiVJrkcnfl7XsBxIZLba1K4";
-let SUPABASE_URL = "https://iqepyjqlnlbwtuehrngr.supabase.co";
+// Configuración
+const SUPABASE_URL = "https://iqepyjqlnlbwtuehrngr.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlxZXB5anFsbmxid3R1ZWhybmdyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQzNTg2MDQsImV4cCI6MjA3OTkzNDYwNH0.C-WwzpilT9jUsVXhoOLjDiVJrkcnfl7XsBxIZLba1K4";
 
-// Crear cliente una sola vez
+// Crear y exportar cliente
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
- 
